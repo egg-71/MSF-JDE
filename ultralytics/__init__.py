cac2b22553fd532a5d6bd9f40c8700cc35341b57
Kris <61447217+egg-71@users.noreply.github.com>
2026-04-13 20:18:44 +0800

    Add files via upload

--- ultralytics/__init__.py
+++ ultralytics/__init__.py
@@ -1,30 +1,9 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 license
 
-__version__ = "8.3.6"
-
-import os
-
-# Set ENV Variables (place before imports)
-os.environ["OMP_NUM_THREADS"] = "1"  # reduce CPU utilization during training
-
-from ultralytics.data.explorer.explorer import Explorer
-from ultralytics.models import NAS, RTDETR, SAM, YOLO, FastSAM, YOLOWorld
-from ultralytics.utils import ASSETS, SETTINGS
-from ultralytics.utils.checks import check_yolo as checks
-from ultralytics.utils.downloads import download
-
-settings = SETTINGS
-__all__ = (
-    "__version__",
-    "ASSETS",
-    "YOLO",
-    "YOLOWorld",
-    "NAS",
-    "SAM",
-    "FastSAM",
-    "RTDETR",
-    "checks",
-    "download",
-    "settings",
-    "Explorer",
-)
+from .fastsam import FastSAM
+from .nas import NAS
+from .rtdetr import RTDETR
+from .sam import SAM
+from .yolo import YOLO, YOLOWorld
+from ultralytics.models.yolo.jde import JDETrainer, JDEValidator, JDEPredictor
+__all__ = "YOLO", "RTDETR", "SAM", "FastSAM", "NAS", "YOLOWorld"  # allow simpler import
